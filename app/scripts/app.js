50d41ae6d2c96e1156894f541b419951af032daf
Rahul <rahul91341@gmail.com>
2016-12-15 22:36:56 +0530

    complete admin pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,14 +10,30 @@ $(function () {
     });
 
     $('#productPhoto').change(function () {
-        readURL(this,'#product-photo');
+        readURL(this, '#product-photo');
     });
-     $('#productDiagram').change(function () {
-        readURL(this,'#product-img');
+    $('#productDiagram').change(function () {
+        readURL(this, '#product-img');
+    });
+
+    $('#motor_spec').click(function () {
+        if (this.checked) {
+            $("#motor_colums,#Motor_rows,#create-motor_table").removeAttr("disabled");
+        } else {
+            $("#motor_colums,#Motor_rows,#create-motor_table").attr("disabled", true);
+        }
+    });
+
+     $('#gear_spec').click(function () {
+        if (this.checked) {
+            $("#gear_colums,#gear_rows,#create-gear_table").removeAttr("disabled");
+        } else {
+            $("#gear_colums,#gear_rows,#create-gear_table").attr("disabled", true);
+        }
     });
 });
 
-function readURL(input,img) {
+function readURL(input, img) {
 
     if (input.files && input.files[0]) {
         var reader = new FileReader();
@@ -28,4 +44,39 @@ function readURL(input,img) {
 
         reader.readAsDataURL(input.files[0]);
     }
+}
+
+function createTable(table_id) {
+
+    var mytable = $('<tbody></tbody>');
+    var rows = new Number($("#Motor_rows").val());
+    var cols = new Number($("#motor_colums").val());
+    var tr = [];
+    for (var i = 0; i < rows; i++) {
+        var row = $('<tr></tr>').attr({ class: [].join(' ') }).appendTo(mytable);
+        for (var j = 0; j < cols; j++) {
+            $('<td></td>').html("<input type='text'>").appendTo(row);
+        }
+
+    }
+    $("#MotortableHolder table").html('');
+    mytable.appendTo("#MotortableHolder table");
+
+}
+function creategearTable(table_id) {
+
+    var mytable = $('<tbody></tbody>');
+    var rows = new Number($("#gear_rows").val());
+    var cols = new Number($("#gear_colums").val());
+    var tr = [];
+    for (var i = 0; i < rows; i++) {
+        var row = $('<tr></tr>').attr({ class: [].join(' ') }).appendTo(mytable);
+        for (var j = 0; j < cols; j++) {
+            $('<td></td>').html("<input type='text'>").appendTo(row);
+        }
+
+    }
+    $("#geartableHolder table").html('');
+    mytable.appendTo("#geartableHolder table");
+
 }
